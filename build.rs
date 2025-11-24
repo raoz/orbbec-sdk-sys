8e1f8ddfb9030547e5b6a86819e916a46c4f5f63
Rao Zvorovski <rao.zvorovski@codeduf.eu>
2025-11-24 14:00:25 +0200

    fix packaging by avoiding changing things outside of outdir

--- build.rs
+++ build.rs
@@ -4,14 +4,21 @@ use std::path::PathBuf;
 use cmake::Config;
 
 fn main() {
-    let libdir_path = PathBuf::from("vendor")
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    println!("cargo:rerun-if-changed=vendor/*");
+    let _ = fs_extra::dir::remove(&out_dir.join("vendor"));
+    fs_extra::dir::copy("vendor/", &out_dir, &fs_extra::dir::CopyOptions::new())
+        .expect("Failed to copy vendor directory");
+
+    let libdir_path = out_dir
+        .join("vendor")
         // Canonicalize the path as `rustc-link-search` requires an absolute
         // path.
         .canonicalize()
         .expect("cannot canonicalize path");
 
     let headers_path = libdir_path.join("include/libobsensor/ObSensor.h");
-    let headers_path_str = headers_path.to_str().expect("Path is not a valid string");
 
     let build_destination = Config::new(&libdir_path)
         .define("OB_BUILD_EXAMPLES", "OFF")
@@ -25,7 +32,6 @@ fn main() {
         build_destination.join("lib").display()
     );
     println!("cargo:rustc-link-lib=dylib=OrbbecSDK");
-    println!("cargo:rerun-if-changed={}", headers_path_str);
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -33,7 +39,7 @@ fn main() {
     let bindings = bindgen::Builder::default()
         // The input header we would like to generate
         // bindings for.
-        .header(headers_path_str)
+        .header(headers_path.to_str().unwrap())
         .clang_arg(format!("-I{}", libdir_path.join("include/").display()))
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
@@ -44,7 +50,7 @@ fn main() {
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs");
+    let out_path = out_dir.join("bindings.rs");
     bindings
         .write_to_file(out_path)
         .expect("Couldn't write bindings!");
